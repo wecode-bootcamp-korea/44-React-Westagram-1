bf516e894f698a073cd4f99473df0db1e57a251b
George Kim <ggkim0614@gmail.com>
2023-03-23 16:52:55 +0900

    Add : Mission 1-3, 사용자 입력 데이터 저장 기능, 로그인 버튼 활성화 기능, 댓글 기능 구현

--- src/pages/jungwhanKim/Login/Login.js
+++ src/pages/jungwhanKim/Login/Login.js
@@ -9,9 +9,6 @@ const Login = () => {
     password: '',
   });
 
-  // 아이디와 비밀번호값 모두 충족 시 버튼 오케이 "참" 상태로 변경
-  const [buttonOkay, setButtonOkay] = useState(false);
-
   // 분해 구조 할당으로 각 인풋값 추출
   const { username, password } = inputs;
 
@@ -19,16 +16,9 @@ const Login = () => {
   const navigate = useNavigate();
 
   // 인풋창에 input 이벤트 발생 시 호출되는 함수
-  const onChange = event => {
+  const saveUserId = event => {
     // 이벤트 타겟 인풋에서의 값과 해당 인풋의 이름을 추출
     const { value, name } = event.target;
-    // console.log(value, name);
-
-    if (inputs.username && inputs.password) {
-      console.log(inputs.username);
-      console.log(inputs.password);
-    } else if (inputs.username.length || inputs.password.length <= 0) {
-    }
 
     setInputs({
       ...inputs,
@@ -37,17 +27,12 @@ const Login = () => {
   };
 
   const checkBothInputs = (isUserNameOkay, isPasswordOkay) => {
-    console.log('CHECKING BOTH INPUTS');
     if (isUserNameOkay && isPasswordOkay) {
       navigate('/main-jk');
-    } else {
-      console.log('somthing WRONG');
-      setButtonOkay(false);
     }
   };
 
   const checkUsername = username => {
-    console.log('checking username.....');
     if (username.match(/^\S+@\S+\.\S+$/)) {
       return true;
     } else {
@@ -57,7 +42,6 @@ const Login = () => {
   };
 
   const checkPassword = password => {
-    console.log('checking user password.....');
     if (password.length > 5) {
       return true;
     } else {
@@ -66,15 +50,11 @@ const Login = () => {
     }
   };
 
-  // const onReset = () => {
-  //   setInputs({
-  //     username: '',
-  //     password: '',
-  //   });
-  // };
+  const handleSubmit = event => {
+    event.preventDefault();
 
-  // form이 submit될 때 호출할 preventdefault용 함수
-  const handleSubmit = event => event.preventDefault();
+    checkBothInputs(checkUsername(username), checkPassword(password));
+  };
 
   return (
     <div className="page-container">
@@ -92,14 +72,14 @@ const Login = () => {
               id="idInput"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
-              onInput={onChange}
+              onInput={saveUserId}
             />
             <input
               name="password"
               id="pwInput"
               type="password"
               placeholder="비밀번호"
-              onInput={onChange}
+              onInput={saveUserId}
             />
             {/* <!--로그인 버튼 요소--> */}
 
@@ -107,15 +87,9 @@ const Login = () => {
               name="login"
               id="login"
               // username or passowrd 값이 없을 시 disabled 클래스 부여
-              className={!username || !password ? 'login disabled' : 'login'}
+              className={!username || password < 6 ? 'login disabled' : 'login'}
               // username or passowrd 값이 없을 시 disabled attribute 추가
               disabled={!username || !password}
-              onClick={() =>
-                checkBothInputs(
-                  checkUsername(username),
-                  checkPassword(password)
-                )
-              }
             >
               로그인
             </button>
