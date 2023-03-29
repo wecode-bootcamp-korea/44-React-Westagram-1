15c47807779475fcf1b802afe59738c4bd8f0efa
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-03-29 22:32:25 +0900

    Add: 하트클릭 구현(추기구현)

--- src/pages/sunyoungChoi/Main/Comment/CommentItem.js
+++ src/pages/sunyoungChoi/Main/Comment/CommentItem.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { IoHeartOutline, IoCloseSharp } from 'react-icons/io5';
+import React, { useState } from 'react';
+import { IoHeartOutline, IoCloseSharp, IoHeartSharp } from 'react-icons/io5';
 import './CommentItem.scss';
 
 //8. 자식영역 컴포넌트 생성
 const CommentItem = ({ id, comment, removeComment }) => {
+  const [like, setLike] = useState(false);
+
+  // const clickHeart = () => {
+  //   if(clickCount % 2 !== 0){
+  //       return false;
+  //     } else if ((clickCount % 2 === 0)){
+  //       return true;
+  //     }
+  //   }
+  // }
+
+  //하트의 boolean 구별함수
+  const handleClick = () => {
+    setLike(!like);
+  };
+
   return (
     <div className="comment-item">
       <div className="comment-wrap">
@@ -11,8 +27,17 @@ const CommentItem = ({ id, comment, removeComment }) => {
         <div className="comment-text"> {comment}</div>
       </div>
       <div className="button-wrap">
-        <button className="like-icon">
+        {/* <button className="like-icon" onClick={() => clickHeart()}>
           <IoHeartOutline className="icon" />
+          <IoHeartSharp className="icon" />
+        </button> */}
+        {/* <button onClick={() => setLike(prevLike => !prevLike)}> */}
+        <button className="heart-icon" onClick={handleClick}>
+          {like ? (
+            <IoHeartSharp className="on-heart" />
+          ) : (
+            <IoHeartOutline className="off-heart" />
+          )}
         </button>
         <button className="remove-comment" onClick={() => removeComment(id)}>
           <IoCloseSharp className="icon" />
